748a1683f4ff9df9053c83c0383fa9237f855fb1
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2024-04-25 15:24:33 -0300

    first commit

---
+++ src/main/java/com/jasper/nutriotiontable/models/Nutrition.java
@@ -0,0 +1,25 @@
+package com.jasper.nutriotiontable.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Nutrition {
+    
+    // These are the fields that will be rendered in my JasperReport's Table
+    // Each instante of NutritionClass will be a row in my table
+    private String nutriotionName;
+    private Integer total;
+    private Integer goal;
+    private String metric;
+
+
+
+}
---
+++ src/test/java/com/jasper/nutriotiontable/NutriotionTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasper.nutriotiontable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriotionTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
